cc875114845df4e9e100e0668e877787bb854dee
nitheeshmk5 <nitheeshmk5@gmail.com>
2025-06-20 19:14:01 +0530

    add basic recursions

---
+++ 03-recursion/recur.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void rec(int count){
+    if(count == 5){
+        cout << "end\n";
+        return;
+    }
+    cout << count << endl;
+    count++;
+    rec(count);
+}
+
+void print_name(int i,int n){
+    if(i > n){
+        return;  // Base Condition
+    }
+    cout << "Nitheesh" << endl;
+    print_name(i + 1, n);
+}
+
+void print_to_n(int start,int n){
+    if(start > n){
+        return;
+    }
+    cout << start << endl;
+    print_to_n(start + 1, n);
+}
+
+void print_n_to_one( int n){
+    if (n < 1){
+        return;
+    }
+    cout << n << endl;
+    print_n_to_one(n - 1);
+}
+
+int main(){
+    // int c = 0;
+    // rec(c);
+    //print_name(0,5);
+    print_to_n(0, 5);
+    print_n_to_one(5);
+}
