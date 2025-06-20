e5277e97d0cf0c997c188896a1789f46a1665f57
nitheeshmk5 <nitheeshmk5@gmail.com>
2025-06-20 19:54:02 +0530

    add sum n numbers

---
+++ 03-recursion/sum_N_numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void sumOfNnumbers1(int sum,int n){
+    
+    if (n < 1)
+    {
+        cout << sum << endl;
+        return;
+    }
+    sumOfNnumbers1(sum + n, n - 1);
+}
+
+int sumNum(int n){
+    if(n==0){
+        return 0;
+    }
+    return n + sumNum(n - 1);
+}
+
+int main(){
+    //sumOfNnumbers1(0,10);
+    cout << sumNum(10) << endl;
+}
