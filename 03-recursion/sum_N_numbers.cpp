594bfaf72b672dcdb2b0e0f3cbc51b096216ad56
nitheeshmk5 <nitheeshmk5@gmail.com>
2025-06-21 08:45:03 +0530

    update added fact

--- 03-recursion/sum_N_numbers.cpp
+++ 03-recursion/sum_N_numbers.cpp
@@ -18,7 +18,15 @@ int sumNum(int n){
     return n + sumNum(n - 1);
 }
 
+int fact(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
 int main(){
-    //sumOfNnumbers1(0,10);
-    cout << sumNum(10) << endl;
+    // sumOfNnumbers1(0,10);
+    // cout << sumNum(10) << endl;
+    cout << fact(4) << endl;
 }
